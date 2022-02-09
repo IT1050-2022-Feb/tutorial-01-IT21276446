24aa763f91636e0d993ac6507ed5662303556061
IT21276446 <99312058+IT21276446@users.noreply.github.com>
2022-02-09 06:22:32 +0000

    Completed.

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,29 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+
+  int distance, amount = 0; //declaring variables
+  
+  printf("Enter the distance: "); //getting the input from user
+  scanf("%d", &distance);
+
+  if( distance > 0)
+  {
+    if ( distance <= 30) //calculating the amount for distances less than 30km
+    {
+      amount = distance * 50;
+    }
+
+    else
+    {
+      amount  = 30 * 50 + (distance - 30) * 40; //calculating the amount for distances greater than 30km
+    }
+  }
+
+  else 
+    printf("Invalid distance\n"); //displaying an error message for invalid distances
+
+  printf("Amount = %d\n", amount);//displaying the final amount
   
   return 0;
 }
